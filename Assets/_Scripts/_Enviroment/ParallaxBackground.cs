cd70c0725952fc750b3d6aeba106ac4945dcfcf1
Rendy-4 <rendyjuans2@gmail.com>
2026-04-08 00:35:59 +0700

    membuat scene main menu dan menyelesaikan parallax background

--- Assets/_Scripts/_Enviroment/ParallaxBackground.cs
+++ Assets/_Scripts/_Enviroment/ParallaxBackground.cs
@@ -2,66 +2,44 @@ using UnityEngine;
 
 namespace AstroShift.Environment
 {
+    [System.Serializable]
+    public class BackgroundElement
+    {
+        [SerializeField] private SpriteRenderer backgroundSprite;
+        [Range(0f, 1f)] public float scrollSpeed;
+        
+        [HideInInspector] public Material spriteMaterial;
+        public SpriteRenderer BackgroundSprite => backgroundSprite;
+    }
+
     public class ParallaxBackground : MonoBehaviour
     {
-        private Transform cameraTransform;
-        private Vector3 cameraStartPos;
-        private float distance;
+        private const float SCROLL_MULTIPLIER = 0.01f;
+        [SerializeField] private BackgroundElement[] backgroundElements;
 
-        private GameObject[] background;
-        private Material[] materials;
-        private float[] backgroundSpeed;
-
-        // 1. Perbaikan agar muncul di Inspector
-        [SerializeField] [Range(0.01f, 0.5f)] 
-        private float parallaxSpeed = 0.02f; 
-
-        private float farthestBackground;
-
-        private void Start() {
-            cameraTransform = Camera.main.transform;
-            cameraStartPos = cameraTransform.position;
-
-            int backgroundCount = transform.childCount;
-            materials = new Material[backgroundCount];
-            backgroundSpeed = new float[backgroundCount];
-            background = new GameObject[backgroundCount];
-
-            for (int i = 0; i < backgroundCount; i++) {
-                background[i] = transform.GetChild(i).gameObject;
-                // Gunakan Renderer agar lebih fleksibel (bisa Sprite atau Mesh)
-                materials[i] = background[i].GetComponent<Renderer>().material;
-            }
-
-            // Panggil fungsi hitung kecepatan
-            CalculateBackgroundSpeed(backgroundCount);
-        }
-
-        private void CalculateBackgroundSpeed(int backgroundCount) {
-            farthestBackground = 0;
-
-            // Cari jarak terjauh
-            for (int i = 0; i < backgroundCount; i++) {
-                float zDist = background[i].transform.position.z - cameraTransform.position.z;
-                if (zDist > farthestBackground) {
-                    farthestBackground = zDist;
+        private void Start()
+        {
+            foreach (BackgroundElement element in backgroundElements)
+            {
+                if (element.BackgroundSprite != null)
+                {
+                    // Pastikan material di-assign di awal
+                    element.spriteMaterial = element.BackgroundSprite.material;
                 }
             }
-
-            // Hitung kecepatan berdasarkan jarak Z
-            for (int i = 0; i < backgroundCount; i++) {
-                float zDist = background[i].transform.position.z - cameraTransform.position.z;
-                backgroundSpeed[i] = 1 - (zDist / farthestBackground);
-            }
         }
 
-        private void LateUpdate() {
-            distance = cameraTransform.position.x - cameraStartPos.x;
-
-            for (int i = 0; i < background.Length; i++) {
-                float speed = backgroundSpeed[i] * parallaxSpeed;
-                // Menggerakkan isi tekstur (Offset)
-                materials[i].SetTextureOffset("_MainTex", new Vector2(distance * speed, 0));
+        private void Update()
+        {
+            foreach (BackgroundElement element in backgroundElements)
+            {
+                if (element.spriteMaterial != null)
+                {
+                    // PERBAIKAN: Gunakan '=' dan transform.position.x 
+                    // Bukan '+=' agar sinkron dengan gerak kamera
+                    float offset = transform.position.x * element.scrollSpeed * SCROLL_MULTIPLIER;
+                    element.spriteMaterial.mainTextureOffset = new Vector2(offset, 0f);
+                }
             }
         }
     }
