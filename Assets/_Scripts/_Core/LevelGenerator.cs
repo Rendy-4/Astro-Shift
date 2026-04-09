735ebd531fe108d7eb9799c252010b1d91519ea8
Rendy-4 <rendyjuans2@gmail.com>
2026-04-09 13:35:36 +0700

    procedural spawn #2

--- Assets/_Scripts/_Core/LevelGenerator.cs
+++ Assets/_Scripts/_Core/LevelGenerator.cs
@@ -1,23 +1,42 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace AstroShift.Core
 {
     public class LevelGenerator : MonoBehaviour
     {
+        private const float PLAYER_DISTANCE_SPAWN_LEVEL_PART = 200f;
         [SerializeField] private Transform _levelPartStart;
-        [SerializeField] private Transform _LevelPart1;
+        [SerializeField] private List<Transform> _listPartLevel;
+        [SerializeField] private Transform _playerTransform;
+
+        private Vector3 _lastEndPosition;
+        private int stratingLevelParts = 5;
         private void Awake() {
-            Transform lastLevelPartTransform;
-            lastLevelPartTransform = SpawnLevelPart(_levelPartStart.Find("End Position").position);
-            lastLevelPartTransform = SpawnLevelPart(lastLevelPartTransform.Find("End Position").position);
-            lastLevelPartTransform = SpawnLevelPart(lastLevelPartTransform.Find("End Position").position);
-            lastLevelPartTransform = SpawnLevelPart(lastLevelPartTransform.Find("End Position").position);
-            lastLevelPartTransform = SpawnLevelPart(lastLevelPartTransform.Find("End Position").position);
+            _lastEndPosition = _levelPartStart.Find("End Position").position;
+            for (int i = 0; i < stratingLevelParts; i++)
+            {
+                SpawnLevelPart();
+            }
         }
 
-        private Transform SpawnLevelPart(Vector3 spawnposition)
+        private void Update() {
+            if (Vector3.Distance(_playerTransform.position, _lastEndPosition) < PLAYER_DISTANCE_SPAWN_LEVEL_PART)
+            {
+                SpawnLevelPart();
+            }
+        }
+
+        private void SpawnLevelPart()
         {
-            Transform levelPartTransform = Instantiate(_LevelPart1, spawnposition, Quaternion.identity);
+            Transform randomLevelPart = _listPartLevel[Random.Range(0, _listPartLevel.Count)];
+
+            Transform lastLevelPartTransform = CreatLevelPart(randomLevelPart, _lastEndPosition);
+            _lastEndPosition = lastLevelPartTransform.Find("End Position").position;
+        }
+        private Transform CreatLevelPart(Transform LevelPart, Vector3 spawnposition)
+        {
+            Transform levelPartTransform = Instantiate(LevelPart, spawnposition, Quaternion.identity);
             return levelPartTransform;
         }
     }
