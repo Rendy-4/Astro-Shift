a5565b682044e17a763d8cfb277138602447066a
Rendy-4 <rendyjuans2@gmail.com>
2026-04-25 17:56:20 +0700

    menata map level 1 dan penambahan script di hazard.cs

--- Assets/_Scripts/_Core/HazardScript.cs
+++ Assets/_Scripts/_Core/HazardScript.cs
@@ -1,10 +1,27 @@
 using UnityEngine;
 using AstroShift.Core;
+using AstroShift.Player;
 
 namespace AstroShift.Core
 {
     public class HazardScript : MonoBehaviour
     {
+        private Collider2D _collider;
+        private Transform player;
+
+        private void Start() {
+            _collider = GetComponent<Collider2D>();
+            
+            GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+            player = playerObj.transform;
+        }
+
+        private void Update() {
+            if (player == null || _collider == null) return;
+
+            _collider.enabled = Vector2.Distance(transform.position, player.position) < 15f;
+        }
+
         private void OnCollisionEnter2D(Collision2D other) {
                 IDamageable damageable = other.gameObject.GetComponent<IDamageable>();
                 if (damageable != null)
