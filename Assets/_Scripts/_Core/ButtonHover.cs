de45bf4363d5f126b799377d45c3679f45e0bb19
Rendy-4 <rendyjuans2@gmail.com>
2026-04-26 18:04:03 +0700

    membuat system hover button

---
+++ Assets/_Scripts/_Core/ButtonHover.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+
+namespace AstroShift.Core
+{
+    public class ButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+    {
+        [SerializeField] private float hoverScale = 1.1f;
+        [SerializeField] private float hoverDuration = 0.2f;
+        
+        private Vector3 originalScale;
+
+        private void Awake() {
+            originalScale = transform.localScale;
+        }
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            transform.DOScale(originalScale * hoverScale, hoverDuration)
+                .SetEase(Ease.OutBack)
+                .SetUpdate(true);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            transform.DOScale(originalScale, hoverDuration)
+                .SetEase(Ease.OutBack)
+                .SetUpdate(true);
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            transform.DOKill();
+            transform.DOScale(originalScale * 0.9f, 0.05f)
+                .SetUpdate(true)
+                .OnComplete(() =>
+                    transform.DOScale(originalScale, 0.05f)
+                        .SetUpdate(true)
+                );
+        }
+    }    
+}
---
+++ Assets/_Scripts/_Core/PanelAnim.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using DG.Tweening;
+
+namespace AstroShift.Core
+{
+    public class PanelAnim : MonoBehaviour
+    {
+        public enum AnimType {fromTop, ScaleUP}
+
+        [SerializeField] private AnimType animType;
+        [SerializeField] private float duration = 0.5f;
+        [SerializeField] private float offsetY = 800f;
+
+        private Vector2 originalPos;
+        private Vector3 originalScale;
+        private RectTransform rect;
+
+        private void Awake() {
+            rect = GetComponent<RectTransform>();
+            originalPos = rect.anchoredPosition;
+            originalScale = rect.localScale;
+        }
+
+        private void OnEnable()
+        {
+            if (animType == AnimType.fromTop)
+            {
+                rect.anchoredPosition = new Vector2(originalPos.x, originalPos.y + offsetY);
+                rect.DOAnchorPos(originalPos, duration)
+                    .SetEase(Ease.OutBounce)
+                    .SetUpdate(true);
+                    
+            }
+            else if (animType == AnimType.ScaleUP)
+            {
+                rect.localScale = Vector3.zero;
+                rect.DOScale(originalScale, duration)
+                    .SetEase(Ease.OutBack)
+                    .SetUpdate(true);
+                
+            }
+        }
+    }    
+}
